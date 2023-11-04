d70432157ef210e0e9056443af0ce7096eb5a5c6
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2023-11-04 22:53:00 +0700

    feat: add mysqli-validate-int-injection in mysqli result class folder

---
+++ mysqli-result-class/44-mysqli-validate-int-inject.php
@@ -0,0 +1,18 @@
+<?php
+// SQL Injection
+$_GET['id_barang'] = '16 OR id_barang = 2';
+$id_barang = $_GET['id_barang'];
+
+$mysqli = new mysqli('localhost', 'root', '', 'ilkoom');
+$query = "SELECT * from barang where id_barang = $id_barang";
+$result = $mysqli->query($query);
+
+while($row = $result->fetch_assoc()) {
+    echo $row['id_barang'] . ' | ';
+    echo $row['nama_barang'] . ' | ';
+    echo $row['jumlah_barang'] . ' | ';
+    echo $row['harga_barang'] . ' | ';
+    echo $row['tanggal_update'] . '<br>';
+}
+
+$result->free();
