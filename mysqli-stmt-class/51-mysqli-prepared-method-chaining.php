6befa15028b57d3729996e2d4dcef16f6e2bca2c
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2023-11-09 08:59:45 +0700

    fix: file method chaining in mysqli stmt class folder

--- mysqli-stmt-class/51-mysqli-prepared-method-chaining.php
+++ mysqli-stmt-class/51-mysqli-prepared-method-chaining.php
@@ -1,8 +1,15 @@
 <?php
+$mysqli = new mysqli("localhost", "root", "", "ilkoom");
+
+$stmt = $mysqli->prepare("SELECT * from barang where id_barang = ?");
+$stmt->bind_param("i", $id_barang);
+$id_barang = 1;
+$stmt->execute();
+
 $row = $stmt->get_result()->fetch_object();
 
-echo $row->id_barang;			echo "	|	";
-echo $row->nama_barang;			echo "	|	";
-echo $row->jumlah_barang;		echo "	|	";
-echo $row->harga_barang;		echo "	|	";
-echo $row->tanggal_update;		
+echo $row->id_barang . " | ";
+echo $row->nama_barang . " | ";
+echo $row->jumlah_barang . " | ";
+echo $row->harga_barang . " | ";
+echo $row->tanggal_update . "<br>";	
