74b4c8d5eeca31c284185755ef3b851ae51d99ac
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2023-11-09 09:07:49 +0700

    fix: file prepared fetch(54) in mysqli stmt class folder

--- mysqli-stmt-class/54-mysqli-prepared-fetch.php
+++ mysqli-stmt-class/54-mysqli-prepared-fetch.php
@@ -1,9 +1,17 @@
 <?php
+$mysqli = new mysqli("localhost", "root", "", "ilkoom");
+
+$stmt = $mysqli->prepare("SELECT * FROM barang WHERE id_barang = ?");
+$stmt->bind_param("i", $id_barang);
+$id_barang = 2;
+
+$stmt->execute();
+
 $stmt->bind_result($id_barang, $nama_barang, $jumlah_barang, $harga_barang, $tanggal_update);
 $stmt->fetch();
 
-echo $id_barang;		echo "	|	";
-echo $nama_barang;		echo "	|	";
-echo $jumlah_barang;	echo "	|	";
-echo $harga_barang;		echo "	|	";
+echo $id_barang . " | ";
+echo $nama_barang . " | ";
+echo $jumlah_barang . " | ";
+echo $harga_barang . " | ";
 echo $tanggal_update;	
