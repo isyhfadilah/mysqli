4c8ed4589a131bcf859af6fef4d04e240716b5b2
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2023-11-09 09:04:04 +0700

    fix: file prepared fetch in mysqli stmt class folder

--- mysqli-stmt-class/53-mysqli-prepared-fetch.php
+++ mysqli-stmt-class/53-mysqli-prepared-fetch.php
@@ -3,14 +3,14 @@ $mysqli = new mysqli("localhost", "root", "", "ilkoom");
 
 $stmt = $mysqli->prepare("SELECT * FROM barang WHERE id_barang = ?");
 $stmt->bind_param("i", $id_barang);
-$id_barang = 5;
+$id_barang = 2;
 
 $stmt->execute();
 
 $stmt->bind_result($a, $b, $c, $d, $e);
 $stmt->fetch();
 
-echo $a."	|	". $b."	|	". $c."	|	". $d. "	|	". $e;
+echo $a." | ". $b." | ". $c." | ". $d. " | ". $e;
 
 $stmt->free_result();
 $stmt->close();
