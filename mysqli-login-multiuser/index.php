4526bf6db682519b145fc50fe187c41cd4f69870
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2024-01-01 21:38:13 +0700

    fix: property name in elemen input

--- mysqli-login-multiuser/index.php
+++ mysqli-login-multiuser/index.php
@@ -12,8 +12,8 @@ require_once("function/koneksi.php");
 </head>
 <body>
     <form method="POST" action="<?= BASE_URL . 'process/process-login.php' ?>">
-        <input type="text" placeholder="username">
-        <input type="text" placeholder="password">
+        <input name="username" type="username" placeholder="username">
+        <input name="password" type="password" placeholder="password">
         <button type="submit">Login</button>
     </form>
 </body>
