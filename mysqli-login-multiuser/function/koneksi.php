0026061616c1437f859b1ce4c2a966bd9f7012d2
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2024-01-02 21:05:58 +0700

    feat: add documentation login multiuser in mysqli documentation

--- mysqli-login-multiuser/function/koneksi.php
+++ mysqli-login-multiuser/function/koneksi.php
@@ -5,4 +5,8 @@ $username = "root";
 $password = "";
 $dbname = "autentikasi-multiuser";
 
+// procedural style
 $mysqli = mysqli_connect($server, $username, $password, $dbname) or die("Koneksi database gagal");
+
+// object oriented style
+// $mysqli = new mysqli($server, $username, $password, $dbname);
